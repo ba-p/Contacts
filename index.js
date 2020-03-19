ff106564d45111b05a2657257baa090507b7b159
ba-p <phuongtr4916@gmail.com>
2020-03-19 18:16:23 +0700

    Fix bug in call function

--- index.js
+++ index.js
@@ -21,21 +21,90 @@ function loadData(){
 }
 
 function create(){
-    var name = readlineSync.question('Name ');
-    var phone = readlineSync.question('Phone ');
+    var name = readlineSync.question('Name: ');
+    var phone = readlineSync.question('Phone: ');
     var contact = {
         name: name,
         phone: phone
     };
-    contacts.push(contact);
+    var phoneC = {
+        name : name,
+        times: 0
+    };
+    var message = {
+        name : name,
+        content : ''
+    };
+    phones.push(phoneC);
+    users.push(contact);
+    mess.push(message);
+    console.log("created");
+    
 }
 
 
 function callLogs(){
-    var data = fs.readFileSync('./database/phone.json');
-    var phones = JSON.parse(data);
-    console.log(phones);
+    console.log(phones.name + ' ' + phones.times);
     showMenu();
+}
+
+function call(name){
+    for(var i = 0; i < phones.length; ++i)
+    {
+        if(phones[i].name == name)
+        {
+            phones[i].times++;
+            break;
+        }
+    }
+}
+
+function message(name){
+    var content = readlineSync.question('Content: ');
+    for(var i = 0;i < mess.length; ++i)
+    {
+        if(mess[i].name == name)
+        {
+            mess[i].content = content;
+        }
+    }
+}
+
+function deleteCon(name){
+    for(var i = 0; i < users.length; ++i)
+    {
+        users.splice(i,1);
+    }
+    for(var i = 0; i < phones.length; ++i)
+    {
+        phones.splice(i,1);
+    }
+    for(var i = 0; i < mess.length; ++i)
+    {
+        mess.splice(i,1);
+    }
+}
+
+function showAction(name){
+    console.log('Options:');
+    console.log('1.Call');
+    console.log('2.Message');
+    console.log('3.Delete');
+    var option = readlineSync.question('> ');
+    switch(option){
+        case '1':
+            call(name);
+            break;
+        case '2':
+            message(name);
+            break;
+        case '3':
+            deleteCon(name);
+            break;
+        default:
+            console.log('Wrong option!');
+            break;
+    }
 }
 
 function searchN(){  
@@ -46,36 +115,34 @@ function searchN(){
         {
             console.log(users[i].name);
             console.log(users[i].phone);
-            showAction();
+            showAction(users[i].name);
             break;
         }
     }
+    
 }
 
 function searchP(){
     var phone = readlineSync.question('Phone: ');
+    var tmp = 0;
     for(var i = 0; i < users.length; ++i)
     {
         if(users[i].phone == phone)
         {
             console.log(users[i].name);
             console.log(users[i].phone);
-            showAction();
+            tmp = 1;
+            showAction(users[i].name);
             break;
         }
     }
-}
-
-function showAction(){
-    console.log('Options:');
-    console.log('1.Call');
-    console.log('')
+    
 }
 
 function search(){
     console.log('Search by: ');
-    console.log('1.Name: ');
-    console.log('2.Phone: ');
+    console.log('1.Name ');
+    console.log('2.Phone ');
     var option = readlineSync.question('> ');
     switch(option){
         case '1':
@@ -90,9 +157,18 @@ function search(){
     }
 }
 
+function save(){
+    var content = JSON.stringify(users);
+    fs.writeFileSync('./database/users.json',content);
+    var content = JSON.stringify(phones);
+    fs.writeFileSync('./database/phone.json',content);
+    var content = JSON.stringify(mess);
+    fs.writeFileSync('./database/messjson',content);
+}
+
 
 function showMenu(){
-    loadData();
+
     console.log('Options: ');
     console.log('1.Call logs')//call times
 //change
@@ -105,15 +181,19 @@ function showMenu(){
     switch(options){
         case '1':
             callLogs();
+            showMenu();
             break;
         case '2':
             create();
+            showMenu();
             break;
         case '3':
             search();
+            showMenu();
             break;
         case '4':
             save();
+            showMenu();
             break;
         default:
             console.log('Wrong option!');
@@ -121,5 +201,12 @@ function showMenu(){
             break;
     }
 }
-showMenu();
+function main(){
+    loadData();
+    showMenu();
+}
+main();
+
+
+
 
